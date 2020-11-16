a63f920dfc005f2ee6496a513da4307a0acc4093
Sin Wildy <sw.wildy@gmail.com>
2020-11-16 03:34:53 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -4,7 +4,7 @@ def find_item_by_name_in_collection(name, collection)
   # Consult README for inputs and outputs
   counter = 0 
   while counter < collection.length 
-    collection[counter]
+    if collection[counter][:item] = name 
     counter += 1 
   end 
 end

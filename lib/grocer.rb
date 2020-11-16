90b30a10589f0f60892dd7364d65c8a3ae91f526
Sin Wildy <sw.wildy@gmail.com>
2020-11-16 03:54:15 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -19,7 +19,7 @@ def consolidate_cart(cart)
   new_cart = []
   counter = 0 
   while counter < cart.length
-    new_cart_items = find_item_by_name_in_collection
+    new_cart_items = find_item_by_name_in_collection(cart[counter][:item], cart)
   end
 
 end
